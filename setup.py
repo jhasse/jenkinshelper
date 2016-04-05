7c451f0708c474e02a6c07f40a3b1030bb3d473a
Jan Niklas Hasse <jhasse@bixense.com>
2016-04-05 15:48:29 +0200

    v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jenkinshelper',
-    version='1.0',
+    version='1.0.1',
     packages=find_packages(),
     author="Jan Niklas Hasse",
     author_email="jhasse@bixense.com",
